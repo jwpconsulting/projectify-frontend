2a3986cc8001237de51ce21ae59d29784542b2ab
Giosué Russo <josh@hizen.io>
2022-06-06 10:48:32 +0900

    Date zero fill

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -36,10 +36,10 @@ export function dateStringToLocal(dateStr: string, time = false): string {
         throw new Error(`Invalid date: ${dateStr}`);
     }
 
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    const fDate = `${year}-${month + 1}-${day}`;
+    const year = String(date.getFullYear()).padStart(4, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const fDate = `${year}-${month}-${day}`;
 
     if (time) {
         return `${fDate} ${date.toLocaleTimeString(lang)}`;
