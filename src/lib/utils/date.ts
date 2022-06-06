9b3d52093b39794094977a72b01d445f6a14c56b
Giosué Russo <josh@hizen.io>
2022-06-06 11:07:14 +0900

    Data lint fix

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -36,9 +36,9 @@ export function dateStringToLocal(dateStr: string, time = false): string {
         throw new Error(`Invalid date: ${dateStr}`);
     }
 
-    const year = String(date.getFullYear()).padStart(4, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const year = String(date.getFullYear()).padStart(4, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
     const fDate = `${year}-${month}-${day}`;
 
     if (time) {
